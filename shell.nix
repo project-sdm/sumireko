af5afc61b0440132356ef8faf9d0ce17c923e489
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-28 16:26:36 -0500

    chore: add missing dependencies to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,12 +3,14 @@
 }:
 pkgs.mkShell {
   packages = with pkgs; [
+    ffmpeg
     (python314.withPackages (
       ps: with ps; [
         numpy
         fastapi
         fastapi-cli
         opencv4
+        librosa
         matplotlib
         python-multipart
         faiss
