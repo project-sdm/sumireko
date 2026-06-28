385e7bb5947b3567769cc01a9181cf6ed3434b50
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-28 16:29:11 -0500

    feat: add basic utilities

---
+++ app/common/utils.py
@@ -0,0 +1,21 @@
+import os
+
+from app.common.exceptions import MissingEnvVariableException
+
+
+def load_env(var: str) -> str:
+    env = os.getenv(var)
+
+    if not env:
+        raise MissingEnvVariableException(var)
+
+    return env
+
+
+def load_env_or(var: str, or_value: str) -> str:
+    return os.environ.get(var, or_value)
+
+
+def unwrap[T](val: T | None) -> T:
+    assert val is not None
+    return val
