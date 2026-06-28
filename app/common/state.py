d0c3318b984b09f4abd4a23a6a01818b13bf52e7
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-28 16:38:44 -0500

    refactor: re-organize and decouple structure

---
+++ app/common/state.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+import cv2
+import faiss
+import numpy as np
+import numpy.typing as npt
+from cv2.typing import MatLike
+from starlette.datastructures import State
+
+
+@dataclass
+class PreprocessedData:
+    media_files: list[str]
+    word_index: faiss.IndexFlatL2
+    words: MatLike
+    index: list[list[tuple[int, float]]]
+    df: npt.NDArray[np.float32]
+    lengths: npt.NDArray[np.float32]
+
+
+@dataclass
+class AppState(State):
+    sift: cv2.SIFT
+    image_data: PreprocessedData
+    audio_data: PreprocessedData
--- app/main.py
+++ app/main.py
@@ -1,67 +1,74 @@
 import json
-import logging
-import math
 from contextlib import asynccontextmanager
-from dataclasses import dataclass
 from pathlib import Path
-from typing import cast
 
 import cv2
 import faiss
 import numpy as np
-import numpy.typing as npt
-from cv2.typing import MatLike
-from fastapi import FastAPI, HTTPException, Request, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from starlette.datastructures import State
 
-import common.image
+from app.audio.router import audio_router
+from app.common.logger import APP_LOGGER
+from app.common.state import PreprocessedData
+from app.images.router import image_router
 
-PREPROCESSED_DIR = Path(".data/images")
-
-
-@dataclass
-class AppState(State):
-    data: PreprocessedData
+PREPROCESSED_DIR = Path(".data")
 
 
-@dataclass
-class PreprocessedData:
-    sift: cv2.SIFT
-    image_files: list[str]
-    word_index: faiss.IndexFlatL2
-    words: MatLike
-    index: list[list[tuple[int, float]]]
-    df: npt.NDArray[np.float32]
-    lengths: npt.NDArray[np.float32]
+def load_image_preprocessed() -> PreprocessedData:
+    APP_LOGGER.info("[IMAGE] Loading preprocessed data...")
+    IMAGE_DIR = f"{PREPROCESSED_DIR}/images"
 
+    words = np.load(f"{IMAGE_DIR}/words.npy")
+    df = np.load(f"{IMAGE_DIR}/df.npy")
+    lengths = np.load(f"{IMAGE_DIR}/lengths.npy")
 
-logger = logging.getLogger("uvicorn.error")
+    word_index: faiss.IndexFlatL2 = faiss.read_index(f"{IMAGE_DIR}/word_index.faiss")
 
+    with open(f"{IMAGE_DIR}/media_files.json") as f:
+        media_files = json.load(f)
 
-def load_preprocessed() -> PreprocessedData:
-    logger.info("[IMAGE] Loading preprocessed data...")
-
-    words = np.load(f"{PREPROCESSED_DIR}/words.npy")
-    df = np.load(f"{PREPROCESSED_DIR}/df.npy")
-    lengths = np.load(f"{PREPROCESSED_DIR}/lengths.npy")
-
-    word_index: faiss.IndexFlatL2 = faiss.read_index(
-        f"{PREPROCESSED_DIR}/word_index.faiss"
-    )
-
-    with open(f"{PREPROCESSED_DIR}/image_files.json") as f:
-        image_files = json.load(f)
-
-    with open(f"{PREPROCESSED_DIR}/index.json") as f:
+    with open(f"{IMAGE_DIR}/index.json") as f:
         index = json.load(f)
 
-    logger.info(f"[IMAGE] Loaded {len(image_files)} images, {len(words)} visual words.")
+    APP_LOGGER.info(
+        f"[IMAGE] Loaded {len(media_files)} images, {len(words)} visual words."
+    )
 
     return PreprocessedData(
-        sift=cv2.SIFT.create(),
-        image_files=image_files,
+        media_files=media_files,
+        word_index=word_index,
+        words=words,
+        index=index,
+        df=df,
+        lengths=lengths,
+    )
+
+
+def load_audio_preprocessed() -> PreprocessedData:
+    APP_LOGGER.info("[AUDIO] Loading preprocessed data...")
+    AUDIO_DIR = f"{PREPROCESSED_DIR}/audios"
+
+    words = np.load(f"{AUDIO_DIR}/words.npy")
+    df = np.load(f"{AUDIO_DIR}/df.npy")
+    lengths = np.load(f"{AUDIO_DIR}/lengths.npy")
+
+    word_index: faiss.IndexFlatL2 = faiss.read_index(f"{AUDIO_DIR}/word_index.faiss")
+
+    with open(f"{AUDIO_DIR}/media_files.json") as f:
+        media_files = json.load(f)
+
+    with open(f"{AUDIO_DIR}/index.json") as f:
+        index = json.load(f)
+
+    APP_LOGGER.info(
+        f"[AUDIO] Loaded {len(media_files)} audios, {len(words)} visual words."
+    )
+
+    return PreprocessedData(
+        media_files=media_files,
         word_index=word_index,
         words=words,
         index=index,
@@ -72,16 +79,18 @@ def load_preprocessed() -> PreprocessedData:
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    app.state.data = load_preprocessed()
+    app.state.sift = cv2.SIFT.create()
+    app.state.image_data = load_image_preprocessed()
+    app.state.audio_data = load_audio_preprocessed()
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.mount(
-    "/fashion-dataset/images",
-    StaticFiles(directory="fashion-dataset/images"),
-    name="static",
+    "/media/images",
+    StaticFiles(directory="media/images"),
+    name="images",
 )
 
 app.add_middleware(
@@ -98,49 +107,4 @@ async def health():
     return {"status": "ok"}
 
 
-@app.post("/image-search")
-async def image_search(req: Request, file: UploadFile, k: int | None = 5):
-    state = cast(AppState, req.app.state)
-    data = state.data
-
-    q_contents = await file.read()
-    q_nparr = np.frombuffer(q_contents, np.uint8)
-
-    q_img = cv2.imdecode(q_nparr, cv2.IMREAD_GRAYSCALE)
-    if q_img is None:
-        raise HTTPException(status_code=400, detail="Could not read image")
-
-    q_img = common.image.downscale(q_img)
-
-    _, q_desc = data.sift.detectAndCompute(q_img, None)
-
-    _, labels = data.word_index.search(q_desc, 1)
-    q_hist = np.bincount(labels.ravel(), minlength=len(data.words))
-
-    n = len(data.image_files)
-
-    def weight(word_id: int, tf: int) -> float:
-        return math.log(1 + tf) * math.log((n + 1) / (data.df[word_id] + 1))
-
-    scores: dict[int, float] = {}
-    query_len_sq = 0.0
-
-    for word_id, tf_query in enumerate(q_hist):
-        if tf_query == 0:
-            continue
-
-        w_query = weight(word_id, tf_query)
-        query_len_sq += w_query**2
-
-        for img_id, w_img in data.index[word_id]:
-            scores[img_id] = scores.get(img_id, 0) + w_img * w_query
-
-    query_length = math.sqrt(query_len_sq)
-
-    for img_id in scores:
-        scores[img_id] /= data.lengths[img_id] * query_length
-
-    result = sorted(scores.items(), key=lambda tup: tup[1], reverse=True)
-    top_files = [data.image_files[i] for i, _ in result[:k]]
-
-    return {"results": [f"{path}" for path in top_files]}
+app.include_router(image_router)
