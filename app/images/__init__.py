7cacaa6d32a56f0dcd84aa18729de1860291348e
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-29 16:40:14 -0500

    chore: remove unused module

--- app/images/__init__.py
+++
@@ -1,9 +0,0 @@
-from cv2.typing import MatLike
-import cv2
-
-import shared.image
-
-
-def image_search(sift: cv2.SIFT, img: MatLike):
-    img = shared.image.downscale(img)
-    _, descriptors = sift.detectAndCompute(img, None)
