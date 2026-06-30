a589d48888426e9b186b8ca32539900dbbad922f
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-29 23:48:06 -0500

    feat: add tests module

---
+++ tests/lib.py
@@ -0,0 +1,79 @@
+import os
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+
+import requests
+from shared.utils import load_env_or
+
+BASE_URL = load_env_or("TEST_API_BASE_URL", "http://localhost:8000")
+
+
+@dataclass
+class SearchResult:
+    results: list[str]
+    time_ms: float
+
+
+class SearchMode(str, Enum):
+    native = "native"
+    pg_brute = "pg-brute"
+    pg_ivf = "pg-ivf"
+    pg_hnsw = "pg-hnsw"
+
+
+def request(path: Path, media_type: str, mode: str, k: int) -> SearchResult:
+    req = requests.post(
+        f"{BASE_URL}/{media_type}/search?mode={mode}&k={k}",
+        files={"file": open(path, "rb")},
+    )
+
+    if not req.ok:
+        raise Exception(f"Request failed: {req.json()}")
+
+    obj = req.json()
+    return SearchResult(obj["results"], obj["time_ms"])
+
+
+def run_test(
+    media_dir: Path, media_type: str, modes: list[str], n_files: int | None, k: int
+) -> dict[str, float]:
+    filenames = os.listdir(media_dir)
+
+    result = dict[str, float]()
+
+    for i, filename in enumerate(filenames):
+        if n_files is not None and i == n_files:
+            break
+
+        path = media_dir / filename
+
+        print(f"file path: {path}")
+        for mode in modes:
+            res = request(path, media_type, mode, k)
+            result[mode] = result.get(mode, 0.0) + res.time_ms
+
+            print(f"[{mode}] {res}")
+
+    n_files = n_files or len(filenames)
+    return {k: (v / n_files) for k, v in result.items()}
+
+
+def bench(
+    media_dir: Path,
+    media_type: str,
+    n_iters: int,
+    n_files: int | None,
+    k: int,
+) -> dict[str, float]:
+
+    modes = [mode.value for mode in SearchMode]
+    bench_res = dict[str, float]()
+
+    for _ in range(n_iters):
+        res = run_test(media_dir, media_type, modes, n_files, k)
+
+        for mode in modes:
+            bench_res[mode] = bench_res.get(mode, 0.0) + res[mode]
+
+    return {k: (v / n_iters) for k, v in bench_res.items()}
---
+++ tests/main.py
@@ -0,0 +1,30 @@
+import argparse
+from pathlib import Path
+from pprint import pprint
+
+from tests.lib import bench
+
+
+def parse_args() -> tuple[Path, str, int, int | None, int]:
+    parser = argparse.ArgumentParser()
+
+    _ = parser.add_argument("media_dir", type=str)
+    _ = parser.add_argument("media_type", choices=["images", "audio"], type=str)
+    _ = parser.add_argument("-i", "--n_iters", default=4, type=int)
+    _ = parser.add_argument("-n", "--n_files", default=None, type=int)
+    _ = parser.add_argument("-k", "--k", default=5, type=int)
+
+    args = parser.parse_args()
+
+    return Path(args.media_dir), args.media_type, args.n_iters, args.n_files, args.k
+
+
+def main():
+    media_dir, media_type, n_iters, n_files, k = parse_args()
+
+    results = bench(media_dir, media_type, n_iters, n_files, k)
+    pprint(results)
+
+
+if __name__ == "__main__":
+    main()
