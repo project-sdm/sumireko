984192cb2d833042818544e6f0c409ea596daebb
Joaquin Lopez <jadrian.ldc@proton.me>
2026-07-01 11:53:12 -0500

    Revert "fix: use valid k param in text-search request script"
    This reverts commit 97ee0f674f5fa9e7dd5b5726931fcaf16d5dded2.

--- requests/text-search.sh
+++ requests/text-search.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl -G --data-urlencode "q=$1" --data-urlencode "k=$2" "http://localhost:8000/text/search"
+
+curl -G --data-urlencode "q=$1" --data-urlencode "mode=$2" "http://localhost:8000/text/search"
