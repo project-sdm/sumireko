77fcdbaac990d066d66e3695917e8de4846c9b0f
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-30 23:45:37 -0500

    feat: update text-search request script

--- requests/text-search.sh
+++ requests/text-search.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 
-curl -G --data-urlencode "q=$1" "http://localhost:8000/text/search"
+curl -G --data-urlencode "q=$1" --data-urlencode "mode=$2" "http://localhost:8000/text/search"
