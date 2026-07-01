97ee0f674f5fa9e7dd5b5726931fcaf16d5dded2
Mathias Castro <mathias.castro@utec.edu.pe>
2026-07-01 09:15:10 -0500

    fix: use valid k param in text-search request script

--- requests/text-search.sh
+++ requests/text-search.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
-curl -G --data-urlencode "q=$1" --data-urlencode "mode=$2" "http://localhost:8000/text/search"
+curl -G --data-urlencode "q=$1" --data-urlencode "k=$2" "http://localhost:8000/text/search"
